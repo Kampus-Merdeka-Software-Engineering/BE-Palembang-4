72798ef2f8eb8839c9392c8b6b36b931dc4e2aab
Azza Nadhifa <114301324+Aleeya28@users.noreply.github.com>
2023-10-10 10:20:07 +0700

    Delete models directory
    revisi

--- models/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Users } from './Users.js';
-export { default as Category } from './Category.js';
-export { default as KelolaPesanan } from './KelolaPesanan.js';
-export { default as PaymentMethod } from './PaymentMethod.js';
-export { default as Product } from './Product.js';
-export { default as Cart } from './Cart.js';
-export { default as DaftarPesanan } from './DaftarPesanan.js';
