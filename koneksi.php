234bf5f9c26b49668d90f285b9233eea3c2c543e
Istqmh07 <142769632+Istqmh07@users.noreply.github.com>
2023-10-10 12:07:52 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php
+
+$_SERVER = "localhost:3306";
+$username = "root";
+$password = "";
+$database_name = "just trift";
+
+$koneksi = mysqli_connect ($server,$username,$password,$database_name);
+
+
+?>
